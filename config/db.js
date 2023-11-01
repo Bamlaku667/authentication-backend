d4090408a0d3d5d20fd418802a97a5ea65e4a9b4
Bamlaku <bamlakhiruya@gmail.com>
2023-11-01 20:24:25 +0300

    nodmon

---
+++ config/db.js
@@ -0,0 +1,12 @@
+// // config/db.js
+// const mongoose = require("mongoose");
+
+// const connectToDB = () => {
+//   const dbURI = process.env.dbURI;
+
+//   return mongoose.connect(dbURI, {
+    
+//   });
+// };
+
+// module.exports = connectToDB;
